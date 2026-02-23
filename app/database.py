ab60bc5176e6b7ef43e41893579a9dda1805b4fa
oneshotdev98 <128334921+oneshotdev98@users.noreply.github.com>
2026-02-23 22:58:25 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,27 +1,22 @@
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import declarative_base
 from app.config import settings
 
-engine = create_engine(settings.DATABASE_URL, echo=False, future=True)
+engine = create_engine(settings.DATABASE_URL, echo=False)
 
 SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
     autocommit=False,
+    autoflush=False,
+    bind=engine
 )
 
-
-class Base(DeclarativeBase):
-    pass
+Base = declarative_base()
 
 
 def get_db():
     db = SessionLocal()
     try:
         yield db
-        db.commit()
-    except:
-        db.rollback()
-        raise
     finally:
         db.close()
